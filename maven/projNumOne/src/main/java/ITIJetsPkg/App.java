07659a780037297fc33671f637856d6bb74309a2
YousefSayed <yousef16992@gmail.com>
2023-01-01 11:57:12 +0200

    add maven lab

---
+++ maven/projNumOne/src/main/java/ITIJetsPkg/App.java
@@ -0,0 +1,13 @@
+package ITIJetsPkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
